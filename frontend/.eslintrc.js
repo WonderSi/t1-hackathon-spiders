5d66d6b62653cd566836470d3e3fb65cdbe3391a
WonderSi <kirillkozlov00@gmail.com>
2025-11-24 17:29:52 +0700

    chore(config): setup project dependencies and build configurations

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
