5fb47959e98b0dc644042cf11cc2f694a42160ef
WonderSi <kirillkozlov00@gmail.com>
2025-11-25 23:20:21 +0700

    refactor(app): update additional configuration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <Header />
-  <div class="main-layout">
-    <Chat />
-    <CodeWindow />
-  </div>
+  <router-view />
 </template>
 
 <script setup lang="ts">
-import Header from './components/Header.vue';
-import Chat from './components/Chat.vue';
-import CodeWindow from './components/CodeWindow.vue';
+
 </script>
 
-<style lang="scss">
-@use "./scss/_base.scss";
-</style>
+<style lang="scss"></style>
